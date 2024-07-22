087982aed16b13e881c69b02de0ea64870d800cc
mlh2nd <mlh2nd@gmail.com>
2024-07-22 17:17:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,35 @@ import GreekAccents as ga
 
 textoptions = {"SBL GNT":"sblgnt.txt"}
 
-sourcetext = textoptions[st.selectbox("Source text:", textoptions.keys())]
-
 col1, col2 = st.columns(2)
 with col1:
-    practicelength = st.number_input("Number of words to practice per verse:", min_value=1, max_value=5)
+    sourcetext = textoptions[st.selectbox("Source text:", textoptions.keys())]
 with col2:
-    attempts = st.number_input("Number of attempts per verse:", min_value=1, max_value=5)
+    practicelength = st.number_input("Number of words to practice per verse:", min_value=1, max_value=5)
 
 text_data = ga.generate_book_data(sourcetext)
 reference, verse, wordlist = ga.choose_verse(text_data, practicelength)
 before, after, test, answer = ga.generate_practice_text(wordlist, practicelength)
 annotated_text(before, (test, ""), after, f"\n({reference})")
 
+attempts = 1
+
 @st.experimental_fragment
 def quiz_instance(correct_answer, attempts):
     with st.form("input_form"):
         useranswer = st.text_input("Type the highlighted word with correct accents: ")
+        st.write(useranswer)
         submitted = st.form_submit_button("Check answer")
         if submitted:
-            result = ga.check_answer(useranswer, correct_answer)
-            st.write(result)
+            correct = ga.check_answer(useranswer, correct_answer)
+            if correct:
+                annotated_text(("Ὀρθῶς ἀπεκρίθης!","", "#afa"))
+            else:
+                annotated_text((f"Try again!", "", "#faa"))
+                attempts = attempts + 1
+        # show_answer = st.button("Show answer")
+        # if show_answer:
+        #     st.write(f"Correct answer: {correct_answer}")
 
 quiz_instance(answer, attempts)
 
